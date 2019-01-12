2cc6660dc27e46c4c7cfebce699dae62c2cb0ea8
ryuta46 <ryuta46g@gmail.com>
2019-01-13 07:16:42 +0900

    Add depended libraries from nem2-sdk-java to build with local jar file.

--- app/src/main/java/com/ttechsoft/try_nem2_android/MainActivity.kt
+++ app/src/main/java/com/ttechsoft/try_nem2_android/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.ttechsoft.try_nem2_android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.nem.sdk.infrastructure.AccountHttp
+import io.nem.sdk.model.account.Address
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
 
 class MainActivity : AppCompatActivity() {
+    val disposeBag = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val address = Address.createFromRawAddress("SC7A4H-7CYCSH-4CP4XI-ZS4G2G-CDZ7JP-PR5FRG-2VBU")
+        val accountHttp = AccountHttp("http://192.168.10.13:3000")
+        val subscription = accountHttp
+            .getAccountInfo(address)
+            .subscribeOn(Schedulers.io())
+            .subscribe {
+                print(it)
+            }
+
+        disposeBag.add(subscription)
     }
 }
